7cf00c6558b9b0230868a2a3020f1b2dfc36234b
Afam <afam262@gmail.com>
2020-11-09 02:20:23 -0600

    link js to html and add current time to js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var currentDayEl = $("#currentDay");
 
-currentDayEl.text("current time");
+
+currentDayEl.text("Current Time: ");
+
+var dt = new Date();
+var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+
+
+currentDayEl.append(time);
